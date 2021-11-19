d1a4ed36d0a247fd2aa1e9468332a703ecd27050
Brandon Eads <brandon181989@hotmail.com>
2021-11-19 11:45:51 -0800

    add editdrink form

---
+++ src/components/EditDrink.js
@@ -0,0 +1,33 @@
+import React from "react";
+import ReusableForm from "./ReusableForm";
+import PropTypes from "prop-types";
+
+function EditDrink (props) {
+  const {drink} = props;
+
+
+  function handleEditDrinkSubmission(event) {
+    event.preventDefault();
+    props.onEditDrink({
+      name: event.target.name.value,
+      quantity: event.target.quantity.valueAsNumber,
+      description: event.target.description.value,
+      id: drink.id});
+  }
+
+
+  return (
+    <React.Fragment>
+      <ReusableForm
+        formSubmissionHandler={handleEditDrinkSubmission}
+        buttonText="Update Drink"/>
+    </React.Fragment>
+  );
+}
+
+EditDrink.propTypes = {
+  drink: PropTypes.object,
+  onEditDrink: PropTypes.func
+};
+
+export default EditDrink;
