414b5ff1bc926c18e1d56793fceff16fea88e2c8
Brandon Eads <brandon181989@hotmail.com>
2021-11-19 09:48:04 -0800

    add code reusuableform maybe more later?

---
+++ src/components/ReusuableForm.js
@@ -0,0 +1,33 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+
+function ReusuableForm(props) {
+  return (
+    <React.Fragment>
+      <form onSubmit={props.formSubmissionHandler}>
+        <input
+          type='text'
+          name='name'
+          placeholder="Drink Name" />
+        <input
+          type='text'
+          name='quantity'
+          min='0'
+          max='126'
+          placeholder="Pints" />
+        <textarea
+          name='description'
+          placeholder='Description for Drink' />
+        <button type='submit'>Add Drink!</button>
+      </form>
+    </React.Fragment>
+  );
+}
+
+ReusuableForm.propTypes = {
+  formSubmissionHandler: PropTypes.func,
+  buttonText: PropTypes.string
+};
+
+export default ReusuableForm;
